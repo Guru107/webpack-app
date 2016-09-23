da07f55b5dd60250f432bf860705de6d586a1d9b
Guru107 <guruak107@gmail.com>
2016-09-23 14:53:55 +0530

    boilerplate

---
+++ webpack.config.client.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack')
+var path = require('path')
+const PATHS = {
+	MOVIES : path.join(__dirname,'./movies')
+	EVENTS : path.join(__dirname,'./events'),
+	BUILD : path.join(__dirname,'./public')
+}
+module.exports = {
+	target:'web',
+	devtool : 'eval',
+	entry:{
+		movies : PATHS.MOVIES,
+		events : PATHS.EVENTS
+	},
+	output : {
+		filename : '[name].js',
+		path : PATHS.BUILD,
+		publicPath: "/assets/"
+	},
+	module:{
+		loaders:[
+			{
+				test:/\.less$/,
+				loader:'style!css!less',
+				exclude:['/node_modules/']
+			},
+			{
+				test:/\.js$/,
+				loader:'react-hot!babel-loader',
+				exclude:['/node_modules/']
+			}
+		]
+	},
+	plugins:[
+		new webpack.HotModuleReplacementPlugin({quiet:true}),
+		new webpack.optimize.OccurenceOrderPlugin(true)
+	]
+
+
+}
